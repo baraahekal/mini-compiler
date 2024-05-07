9a1d92e45b0650117494da24a20124ecfa8b173c
AhmedSehly20 <ahmed.nasreldin@msa.edu.eg>
2024-05-07 22:59:40 +0300

    Edit The UI

--- frontend/src/Code.jsx
+++ frontend/src/Code.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import { CodeEditorEditable } from "react-code-editor-editable";
 import "highlight.js/styles/gruvbox-dark.css";
-import {Button} from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
 
 const CodeEditor = ({ code, setCode }) => (
-  <CodeEditorEditable
-    value={code}
-    setValue={setCode}
-    width="50vw"
-    height="40vh"
-    language="cpp"
-    inlineNumbers
-    caretColor="yellow"
-  />
+  <div
+    style={{
+      borderRadius: "10px",
+      overflow: "hidden",
+      boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.4)",
+      backgroundColor: "#361e1c",
+    }}
+  >
+    <CodeEditorEditable
+      value={code}
+      setValue={setCode}
+      width="800px"
+      height="500px"
+      language="cpp"
+      inlineNumbers
+      caretColor="yellow"
+    />
+  </div>
 );
 
 export default CodeEditor;
--- frontend/src/Tokenizer.js
+++ frontend/src/Tokenizer.js
@@ -1,99 +1,113 @@
-import React, { useState } from 'react';
-import CodeEditor from './Code';
+import React, { useState } from "react";
+import CodeEditor from "./Code";
 import { Button } from "@nextui-org/react";
-import './Tokenizer.css';
+import "./Tokenizer.css";
+import icon from "./icon.svg";
 
 function Tokenizer() {
-    const [code, setCode] = useState('');
-    const [tokens, setTokens] = useState(null);
+  const [code, setCode] = useState("");
+  const [tokens, setTokens] = useState(null);
 
-    const tokenizeCode = async () => {
-        const response = await fetch('http://localhost:3030/tokenize', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ code })
-        });
-        const data = await response.json();
-        setTokens(data);
-    };
+  const tokenizeCode = async () => {
+    const response = await fetch("http://localhost:3030/tokenize", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ code }),
+    });
+    const data = await response.json();
+    setTokens(data);
+  };
 
-    return (
-        <div className="tokenizer-container">
-            <CodeEditor code={code} setCode={setCode} className="code-editor" />
-            <button onClick={tokenizeCode}>Tokenize</button>
-            {tokens && (
-                <div className="tokenized-data">
-                    <h2>Tokenized Data:</h2>
-                    <ul>
-                        {tokens.symbols.length > 0 && (
-                            <li>
-                                <h3>Symbols:</h3>
-                                <ul>
-                                    {Array.from(tokens.symbols).map((symbol, index) => (
-                                        <li key={index}>{symbol}</li>
-                                    ))}
-                                </ul>
-                            </li>
-                        )}
-                        {tokens.identifiers.length > 0 && (
-                            <li>
-                                <h3>Identifiers:</h3>
-                                <ul>
-                                    {Array.from(tokens.identifiers).map((identifier, index) => (
-                                        <li key={index}>{identifier}</li>
-                                    ))}
-                                </ul>
-                            </li>
-                        )}
-                        {tokens.reserved_words.length > 0 && (
-                            <li>
-                                <h3>Reserved Keywords:</h3>
-                                <ul>
-                                    {Array.from(tokens.reserved_words).map((reserved_word, index) => (
-                                        <li key={index}>{reserved_word}</li>
-                                    ))}
-                                </ul>
-                            </li>
-                        )}
-                        {tokens.variables.length > 0 && (
-                            <li>
-                                <h3>Variables:</h3>
-                                <ul>
-                                    {Array.from(tokens.variables).map((variable, index) => (
-                                        <li key={index}>{variable}</li>
-                                    ))}
-                                </ul>
-                            </li>
-                        )}
-                        {Object.entries(tokens.lists).length > 0 && (
-                            <li>
-                                <h3>Lists:</h3>
-                                <ul>
-                                    {Object.entries(tokens.lists).map(([listName, listItems], index) => (
-                                        <li key={index}>
-                                            <strong>{listName}:</strong> {listItems.join(', ')}
-                                        </li>
-                                    ))}
-                                </ul>
-                            </li>
-                        )}
-                        {tokens.comments.length > 0 && (
-                            <li>
-                                <h3>Comments:</h3>
-                                <ul>
-                                    {tokens.comments.map((comment, index) => (
-                                        <li key={index}>{comment}</li>
-                                    ))}
-                                </ul>
-                            </li>
-                        )}
-                    </ul>
-                </div>
-            )}
-        </div>
-    );
+  return (
+    <div>
+      <div className="header">
+        <img src={icon} alt="Icon" width="40px" height="40px" />
+        <h1>Mini-Compiler</h1>
+      </div>
+      <div className="tokenizer-container">
+        <h2>Please Write Your Code here</h2>
+        <CodeEditor code={code} setCode={setCode} className="code-editor" />
+        <button className="Button" onClick={tokenizeCode}>
+          Tokenize
+        </button>
+        {tokens && (
+          <div className="tokenized-data">
+            <h2>Tokenized Data</h2>
+            <ul className="toknized-list">
+              {tokens.symbols.length > 0 && (
+                <li>
+                  <h3>Symbols</h3>
+                  <ul className="toknized-single">
+                    {Array.from(tokens.symbols).map((symbol, index) => (
+                      <li key={index}>{symbol}</li>
+                    ))}
+                  </ul>
+                </li>
+              )}
+              {tokens.identifiers.length > 0 && (
+                <li>
+                  <h3>Identifiers</h3>
+                  <ul className="toknized-single">
+                    {Array.from(tokens.identifiers).map((identifier, index) => (
+                      <li key={index}>{identifier}</li>
+                    ))}
+                  </ul>
+                </li>
+              )}
+              {tokens.reserved_words.length > 0 && (
+                <li>
+                  <h3>Reserved Keywords</h3>
+                  <ul className="toknized-single">
+                    {Array.from(tokens.reserved_words).map(
+                      (reserved_word, index) => (
+                        <li key={index}>{reserved_word}</li>
+                      )
+                    )}
+                  </ul>
+                </li>
+              )}
+              {tokens.variables.length > 0 && (
+                <li>
+                  <h3>Variables</h3>
+                  <ul className="toknized-single">
+                    {Array.from(tokens.variables).map((variable, index) => (
+                      <li key={index}>{variable}</li>
+                    ))}
+                  </ul>
+                </li>
+              )}
+              {Object.entries(tokens.lists).length > 0 && (
+                <li>
+                  <h3>Lists</h3>
+                  <ul className="toknized-single">
+                    {Object.entries(tokens.lists).map(
+                      ([listName, listItems], index) => (
+                        <li key={index}>
+                          <strong>{listName}:</strong> {listItems.join(", ")}
+                        </li>
+                      )
+                    )}
+                  </ul>
+                </li>
+              )}
+              {tokens.comments.length > 0 && (
+                <li>
+                  <h3>Comments</h3>
+                  <ul className="toknized-single">
+                    {tokens.comments.map((comment, index) => (
+                      <li key={index}>{comment}</li>
+                    ))}
+                  </ul>
+                </li>
+              )}
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Tokenizer;
