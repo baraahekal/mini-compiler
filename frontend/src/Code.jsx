e1849bd50365c8096d59e3246932471ba1dfce78
baraahekal <ibaraahekal@gmail.com>
2024-05-10 05:07:22 +0300

    Changed the code-editor to monaco making us able to show the compile errors within the code-editor

--- frontend/src/Code.jsx
+++ frontend/src/Code.jsx
@@ -1,27 +1,58 @@
-import React from "react";
-import { CodeEditorEditable } from "react-code-editor-editable";
-import "highlight.js/styles/gruvbox-dark.css";
+import React, { useRef } from 'react'; 
+import Editor from '@monaco-editor/react';
 
-const CodeEditor = ({ code, setCode }) => (
-  <div
-    style={{
-      borderRadius: "10px",
-      overflow: "hidden",
-      boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.4)",
-      backgroundColor: "#361e1c",
-    }}
-  >
-    <CodeEditorEditable
-      value={code}
-      setValue={setCode}
-      width="800px"
-      height="500px"
-      language="cpp"
-      inlineNumbers
-      caretColor="yellow"
-      tabSize={4}
-    />
-  </div>
-);
+const CodeEditor = ({ code, setCode }) => {
+  const editorRef = useRef(null);
+  const monacoRef = useRef(null);
+
+  const options = {
+    selectOnLineNumbers: true,
+    roundedSelection: false,
+    readOnly: false,
+    cursorStyle: 'line',
+    automaticLayout: false,
+  };
+
+  const compileCode = async () => {
+    monacoRef.current.editor.setModelMarkers(editorRef.current.getModel(), 'owner', [{
+      startLineNumber: 4,
+      startColumn: 4,
+      endLineNumber: 4,
+      endColumn: 4,
+      severity: monacoRef.current.MarkerSeverity.Error,
+      message: "expected ; at this line"
+    }]);
+  };
+
+  const editorDidMount = (editor, monaco) => {
+    console.log('editorDidMount', editor);
+    editor.focus();
+    editorRef.current = editor;
+    monacoRef.current = monaco;
+    compileCode();
+  };
+
+  const onChange = (newValue, e) => {
+    console.log('onChange', newValue, e);
+    setCode(newValue);
+    compileCode();
+  };
+
+  return (
+    <>
+      <Editor
+        height={window.innerHeight / 2}
+        width={window.innerWidth / 2}
+        defaultLanguage="cpp"
+        theme="vs-dark"
+        defaultValue={code}
+        // options={options}
+        onChange={onChange}
+        onMount={editorDidMount}
+      />
+    </>
+    
+  );
+};
 
 export default CodeEditor;
