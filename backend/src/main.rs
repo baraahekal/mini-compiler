ff18e3aa18f742c4513e253fb6fa95bb77a4de6c
baraahekal <ibaraahekal@gmail.com>
2024-05-12 13:37:19 +0300

    Returning a Vec<Token> to the client-side

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,8 +1,25 @@
 use warp::{Filter, Rejection, Reply};
 use serde::{Serialize, Deserialize};
-use std::collections::{HashSet, HashMap};
+use std::collections::HashMap;
 #[cfg(test)]
 mod tests;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum TokenType {
+    Identifier,
+    Symbol,
+    ReservedWord,
+    Variable,
+    List,
+    Comment,
+    Literal,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Token {
+    token_type: TokenType,
+    value: String,
+}
 
 #[derive(Deserialize)]
 struct Code {
@@ -11,13 +28,7 @@ struct Code {
 
 #[derive(Serialize, Clone)]
 struct Tokens {
-    identifiers: Vec<String>,
-    symbols: Vec<String>,
-    reserved_words: Vec<String>,
-    variables: Vec<String>,
-    lists: HashMap<String, String>,
-    comments: Vec<String>,
-    literals: HashMap<String, String>,
+    tokens: Vec<Token>,
 }
 
 pub struct Scanner {
@@ -30,16 +41,11 @@ impl Scanner {
         Self {
             code,
             tokens: Tokens {
-                identifiers: Vec::new(),
-                symbols: Vec::new(),
-                reserved_words: Vec::new(),
-                variables: Vec::new(),
-                lists: HashMap::new(),
-                comments: Vec::new(),
-                literals: HashMap::new(),
+                tokens: Vec::new(),
             },
         }
     }
+
 
     fn process_comments(&mut self) -> String {
         let mut cleaned_code = String::new();
@@ -50,12 +56,18 @@ impl Scanner {
             if in_multi_line_comment {
                 if let Some(end) = line.find("*/") {
                     in_multi_line_comment = false;
-                    self.tokens.comments.push(line[..end + 2].to_string());
+                    self.tokens.tokens.push(Token {
+                        token_type: TokenType::Comment,
+                        value: line[..end + 2].to_string(),
+                    });
 
                     cleaned_code.push_str(&line[end + 2..]);
                     cleaned_code.push(' ');
                 } else {
-                    self.tokens.comments.push(line.to_string());
+                    self.tokens.tokens.push(Token {
+                        token_type: TokenType::Comment,
+                        value: line.to_string(),
+                    });
                 }
             } else {
                 if let Some(start) = line.find("/*") {
@@ -64,15 +76,24 @@ impl Scanner {
                     cleaned_code.push(' ');
                     if let Some(end) = line.find("*/") {
                         in_multi_line_comment = false;
-                        self.tokens.comments.push(line[start..end + 2].to_string());
+                        self.tokens.tokens.push(Token {
+                            token_type: TokenType::Comment,
+                            value: line[start..end + 2].to_string(),
+                        });
 
                         cleaned_code.push_str(&line[end + 2..]);
                         cleaned_code.push(' ');
                     } else {
-                        self.tokens.comments.push(line[start..].to_string());
+                        self.tokens.tokens.push(Token {
+                            token_type: TokenType::Comment,
+                            value: line[start..].to_string(),
+                        });
                     }
                 } else if let Some(start) = line.find("//") {
-                    self.tokens.comments.push(line[start..].to_string());
+                    self.tokens.tokens.push(Token {
+                        token_type: TokenType::Comment,
+                        value: line[start..].to_string(),
+                    });
                     cleaned_code.push_str(&line[..start]);
                     cleaned_code.push(' ');
                 } else {
@@ -88,13 +109,25 @@ impl Scanner {
     fn process_literals(&mut self) {
         for word in self.code.split(|c: char| c.is_whitespace() || c == ';' || c == '=' || c == '(' || c == ')' || c == '{' || c == '}' || c == ',' || c == '+' || c == '-' || c == '*' || c == '/' || c == '%' || c == '|' || c == '&' || c == '>' || c == '<' || c == '!') {
             if word.starts_with('"') && word.ends_with('"') {
-                self.tokens.literals.insert(word.trim_matches('"').to_string(), "String".to_string());
+                self.tokens.tokens.push(Token {
+                    token_type: TokenType::Literal,
+                    value: word.trim_matches('"').to_string(),
+                });
             } else if word.starts_with('\'') && word.ends_with('\'') && word.len() == 3 {
-                self.tokens.literals.insert(word.to_string(), "Character".to_string());
+                self.tokens.tokens.push(Token {
+                    token_type: TokenType::Literal,
+                    value: word.to_string(),
+                });
             } else if word == "true" || word == "false" {
-                self.tokens.literals.insert(word.to_string(), "Boolean".to_string());
+                self.tokens.tokens.push(Token {
+                    token_type: TokenType::Literal,
+                    value: word.to_string(),
+                });
             } else if word.parse::<i32>().is_ok() || word.parse::<f64>().is_ok() {
-                self.tokens.literals.insert(word.to_string(), "Numeric".to_string());
+                self.tokens.tokens.push(Token {
+                    token_type: TokenType::Literal,
+                    value: word.to_string(),
+                });
             }
         }
     }
@@ -105,10 +138,16 @@ impl Scanner {
         let chars: Vec<char> = self.code.chars().collect();
         for i in 0..chars.len() {
             if (chars[i] == '&') && i + 1 < chars.len() && chars[i] == chars[i + 1] || (chars[i] == '|') && i + 1 < chars.len() && chars[i] == chars[i + 1] {
-                self.tokens.symbols.push(format!("{}{}", chars[i], chars[i]));
+                self.tokens.tokens.push(Token {
+                    token_type: TokenType::Symbol,
+                    value: format!("{}{}", chars[i], chars[i]),
+                });
             }
             else if symbols.contains(&chars[i]) && i > 0 && chars[i - 1] != chars[i] {
-                self.tokens.symbols.push(chars[i].to_string());
+                self.tokens.tokens.push(Token {
+                    token_type: TokenType::Symbol,
+                    value: chars[i].to_string(),
+                });
             }
         }
     }
@@ -119,23 +158,32 @@ impl Scanner {
 
         for word in self.code.split(|c: char| c.is_whitespace() || c == ';' || c == '=' || c == '(' || c == ')' || c == '{' || c == '}' || c == ',' || c == '+' || c == '-' || c == '*' || c == '/' || c == '%' || c == '|' || c == '&' || c == '>' || c == '<' || c == '!' || c == '"' || c == '\'') {
             if identifiers.contains(&word) {
-                self.tokens.identifiers.push(word.to_string());
+                self.tokens.tokens.push(Token {
+                    token_type: TokenType::Identifier,
+                    value: word.to_string(),
+                });
             } else if reserved_words.contains(&word) {
-                self.tokens.reserved_words.push(word.to_string());
+                self.tokens.tokens.push(Token {
+                    token_type: TokenType::ReservedWord,
+                    value: word.to_string(),
+                });
             }
         }
     }
 
-    fn process_variables(&mut self) {
+      fn process_variables(&mut self) {
         #[cfg(test)]
         self.process_identifiers_and_reserved_words();
         use regex::Regex;
         for word in self.code.split(|c: char| c.is_whitespace() || c == ';' || c == '=' || c == '(' || c == ')' || c == '{' || c == '}' || c == ',' || c == '+' || c == '-' || c == '*' || c == '/' || c == '%' || c == '|' || c == '&' || c == '>' || c == '<' || c == '!' || c == '\'') {
             let variable_part = word.trim().to_string();
 
-            // Check if the word is not an identifier or reserved word before classifying it as a variable // word.chars().next().unwrap_or_default().is_alphabetic()
-            if !self.tokens.identifiers.contains(&variable_part) && !self.tokens.reserved_words.contains(&variable_part) && (Regex::new(r"^[a-zA-Z]").unwrap().is_match(&variable_part) || variable_part.starts_with('_')) {
-                self.tokens.variables.push(variable_part);
+            // Check if the word is not an identifier or reserved word before classifying it as a variable
+            if !self.tokens.tokens.iter().any(|token| token.value == variable_part && (matches!(token.token_type, TokenType::Identifier) || matches!(token.token_type, TokenType::ReservedWord))) && (Regex::new(r"^[a-zA-Z]").unwrap().is_match(&variable_part) || variable_part.starts_with('_')) {
+                self.tokens.tokens.push(Token {
+                    token_type: TokenType::Variable,
+                    value: variable_part,
+                });
             }
         }
     }
@@ -155,7 +203,10 @@ impl Scanner {
                         list_initialization_part.split(',').count()
                     };
                     let list_initialization = format!("{{{} (length: {})", list_initialization_part, list_length);
-                    self.tokens.lists.insert(list_declaration.to_string(), list_initialization);
+                    self.tokens.tokens.push(Token {
+                        token_type: TokenType::List,
+                        value: format!("{} {}", list_declaration, list_initialization),
+                    });
 
             }
         }
